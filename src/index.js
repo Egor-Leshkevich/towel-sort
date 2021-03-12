28d80b97d22b3165d59ff9861f7bb9f5d980f778
Egor-Leshkevich <dellg3172018@gmail.com>
2021-03-13 02:46:39 +0300

    feat: implement task function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
+
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix==undefined) {return [];}
+  var i,j,row_to_rev;
+  var end_arr=[]
+  var arr=matrix;
+  row_to_rev=matrix.length;
+  if(row_to_rev%2!=0)
+  row_to_rev=Math.floor(row_to_rev/2);
+  else row_to_rev/=2;
+  for(i=0,j=0;j<row_to_rev;i+=2,j++) {
+  arr[i+1]=arr[i+1].reverse();  
+  }
+  end_arr=arr.flat();
+  return end_arr;
 }
